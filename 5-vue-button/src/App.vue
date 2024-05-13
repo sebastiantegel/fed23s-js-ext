9a4e5571fbe12a9858e74bcf36eccfffe5a3fb91
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2024-05-13 14:16:35 +0200

    ✨ Button with props and emit

---
+++ 5-vue-button/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Button from "./components/Button.vue";
+
+const handleSave = () => {
+  console.log("Spara någonting");
+};
+
+const handleRemove = () => {
+  console.log("Ta bort någonting");
+};
+</script>
+
+<template>
+  <div>
+    <Button button-text="Spara" title="Spara" @on-click="handleSave" />
+  </div>
+  <div>
+    <Button button-text="Ta bort" title="Ta bort" @on-click="handleRemove" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ 5-vue-button/src/components/Button.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+interface ButtonProps {
+  buttonText: string;
+  title: string;
+}
+
+defineProps<ButtonProps>();
+
+defineEmits<{
+  (e: "onClick"): void;
+}>();
+</script>
+
+<template>
+  <button :title="title" @click="$emit('onClick')">{{ buttonText }}</button>
+</template>
+
+<style scoped>
+button {
+  border: none;
+  border-radius: 5px;
+  background-color: blueviolet;
+  padding: 1rem;
+}
+</style>
